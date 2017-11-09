28085ad48c2170b0c582a96d43717bb873bd13e9
Shayne Meyer <shayne.meyer@gmail.com>
2017-11-08 21:54:53 -0800

    added MemoryGame

--- react/memory-game/src/index.js
+++ react/memory-game/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MemoryGame from './MemoryGame';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<MemoryGame />, document.getElementById('root'));
