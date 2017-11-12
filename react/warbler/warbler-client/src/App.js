f974166b6ec4b6546a60ba9a044fa263574ab29c
Shayne Meyer <shayne.meyer@gmail.com>
2017-11-11 16:13:39 -0800

    got sign in and sign out working

--- react/warbler/warbler-client/src/App.js
+++ react/warbler/warbler-client/src/App.js
@@ -3,19 +3,61 @@ import Navbar from './Navbar';
 import './App.css';
 
 class App extends Component {
-
-  handleClickSignUp() {
-
+  constructor(props){
+    super(props)
+    this.state = {
+      userId: '',
+      username: '',
+      token: '',
+      message: '',
+      profileImageUrl: ''
+    }
   }
 
-  handleClickSignIn() {
+  handleClickSignUp = (e) => {
 
+    this.setState({message: "Clicked Sign up"});
+  };
+
+  handleClickSignIn = (e) => {
+    const loginUrlBase = 'http://localhost:8081/api/auth/signin';
+    const payload = {
+      method: 'post',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ "email": "Leola36@yahoo.com", "password": "password"})
+    }
+
+    fetch(loginUrlBase, payload)
+      .then(data => data.json())
+      .then(data => {
+        this.setState({
+          userId: data.userId,
+          message: `Welcome ${data.username}`,
+          username: data.username,
+          token: data.token,
+          profileImageUrl: data.profileImageUrl
+        });
+      });
+  };
+
+  handleClickSignOut = (e) => {
+    this.setState({
+      userId: '',
+      username: '',
+      token: '',
+      message: 'Please log in.',
+      profileImageUrl: ''
+    })
   }
 
   render() {
     return (
       <div>
-        <Navbar handleClickSignUp={this.handleClickSignUp} handleClickSignIn={this.handleClickSignIn} />
+        <Navbar handleClickSignUp={this.handleClickSignUp} handleClickSignIn={this.handleClickSignIn} handleClickSignOut={this.handleClickSignOut} username={this.state.username} />
+        {this.state.message}
       </div>
     );
   }
--- react/warbler/warbler-client/src/Navbar.js
+++ react/warbler/warbler-client/src/Navbar.js
@@ -4,14 +4,16 @@ import './Navbar.css';
 export default class Navbar extends Component {
 
   render() {
+    let { handleClickSignUp, handleClickSignIn, handleClickSignOut, username } = this.props;
+
     return (
       <header>
         <h2>
-          <a href="">Warbler</a>
+          <a >Warbler</a>
         </h2>
         <nav>
-          <li><a>Sign Up</a></li>
-          <li><a>Log In</a></li>
+          <li><a onClick={handleClickSignUp}>Sign Up</a></li>
+          {username.length > 0 ? <li><a onClick={handleClickSignOut}>Log Out</a></li> : <li><a onClick={handleClickSignIn}>Log In</a></li>}
         </nav>
       </header>
     )
